f4ad7d6d3480421e5a30309413795b469adeeb06
ninew <ninewza@gmail.com>
2019-05-25 17:02:52 +0700

    add script

---
+++ src/js/forest.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  console.log("ready")
+});
+$(document).keydown(function(e) {
+  var ew = e.which;
+  switch(ew) {
+      case 37: // left
+      console.log("left")
+      break;
+
+      case 39: // right
+      console.log("right")
+      break;
+      default: return; // exit this handler for other keys
+  }
+  e.preventDefault(); // prevent the default action (scroll / move caret)
+});

ff2f5fe934875e979a0eb7542947fdf6006013a7
ninew <ninewza@gmail.com>
2019-05-25 10:25:45 +0700

    function lazyload

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,23 @@
-var hello = function hello(name) {
-  return "hello ".concat(name);
-};
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var imgs = _toConsumableArray(document.querySelectorAll('.lazy'));
+
+function lazyLoad() {
+  imgs.forEach(function (img) {
+    if (img.offsetTop < window.innerHeight + window.pageYOffset) {
+      img.src = img.dataset.src;
+
+      img.onload = function () {
+        return img.classList.add('loaded');
+      };
+    }
+  });
+}
+
+lazyLoad();
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
-const hello = (name) => {
-  return `hello ${name}`;
-};
+let imgs = [...document.querySelectorAll('.lazy')]
+
+function lazyLoad() {
+  imgs.forEach(img => {
+    if (img.offsetTop < window.innerHeight + window.pageYOffset) {
+        img.src = img.dataset.src
+        img.onload = () => img.classList.add('loaded')
+    }
+  })
+}
+
+lazyLoad()

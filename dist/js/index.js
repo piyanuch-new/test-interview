b6d04d406ea8b5663ed6793b003373ae18675f01
ninew <ninewza@gmail.com>
2019-05-25 11:31:55 +0700

    script lazy load img

--- dist/js/index.js
+++ dist/js/index.js
@@ -8,16 +8,37 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 var imgs = _toConsumableArray(document.querySelectorAll('.lazy'));
 
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+        args = arguments;
+
+    var later = function later() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+;
+
 function lazyLoad() {
   imgs.forEach(function (img) {
-    if (img.offsetTop < window.innerHeight + window.pageYOffset) {
-      img.src = img.dataset.src;
+    console.log(window.innerHeight, img.getBoundingClientRect().top, img.getBoundingClientRect().bottom);
 
-      img.onload = function () {
-        return img.classList.add('loaded');
-      };
+    if (img.getBoundingClientRect().top <= window.innerHeight && img.getBoundingClientRect().bottom >= 0 && getComputedStyle(img).display !== "none") {
+      img.src = img.dataset.src;
+      img.classList.remove("lazy");
     }
   });
 }
 
 lazyLoad();
+window.addEventListener('scroll', debounce(lazyLoad, 16));
+window.addEventListener('resize', debounce(lazyLoad, 16));
